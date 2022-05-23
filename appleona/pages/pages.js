e425a6ffa3ee15f7074f8c067e83543f11655596
René Mayett <44554119+rene-mayett@users.noreply.github.com>
2022-05-23 10:11:02 -0500

    ValidarDatos JS
    Se agrega fun validacion de Datos en login leona.

--- appleona/pages/pages.js
+++ appleona/pages/pages.js
@@ -1,41 +1,53 @@
 let button = document.getElementById('btn-enviar');
 
-button.addEventListener('click', function() {
-    // alert('Mensaje de alerta');
+// button.addEventListener('click', function() {
+//     // alert('Mensaje de alerta');
 
-    // var curp = document.getElementById(input-curp);
-    // var idp = document.getElementById(input-idp);
-    let inputCurp = document.getElementById("input-curp").value;
-    let inputIdp = document.getElementById("input-idp").value;
-    let pMensaje = document.getElementById('p-mensaje-error');
-    let pMensajeConfirmacion = document.getElementById("p-mensaje-confirmacion");
+//     // var curp = document.getElementById(input-curp);
+//     // var idp = document.getElementById(input-idp);
+//     let inputCurp = document.getElementById("curp").nodeValue;
+//     let inputIdp = document.getElementById("idp").nodeValue;
+//     let pMensaje = document.getElementById('p-mensaje-error'); // ???????
+//     let pMensajeConfirmacion = document.getElementById("p-mensaje-confirmacion");
 
 
-    if(inputCurp.value == ""){
-        // alert("Error: El nombre esta vacio.");
-        pMensaje.innerHTML = "Error: El CURP esta vacio"
-    }
+//     if(inputCurp.value == ""){
+//         // alert("Error: El nombre esta vacio.");
+//         pMensaje.innerHTML = "Error: El CURP esta vacio"
+//     }
 
-    else if(inputIdp.value == ""){
-        // alert("Error: El correo esta vacio.");
-        pMensaje.innerHTML = "Error: El IDP esta vacio"
-    }
+//     else if(inputIdp.value == ""){
+//         // alert("Error: El correo esta vacio.");
+//         pMensaje.innerHTML = "Error: El IDP esta vacio"
+//     }
 
-    else if (inputIdp.value.length > 5){
-        pMensaje.innerHTML = "Error: El idp es muy largo."
-    }
+//     else if (inputIdp.value.length > 5){
+//         pMensaje.innerHTML = "Error: El idp es muy largo."
+//     }
 
-    else{
+//     else{
     
-         console.log(inputCurp);
+//          console.log(inputCurp);
 
-    }
+//     }
     
-    // else if (inputCurp.value.length > 18){
-    //     pMensaje.innerHTML = "Error: El CURP excede la longitud permitida."
-    // }
+//     // else if (inputCurp.value.length > 18){
+//     //     pMensaje.innerHTML = "Error: El CURP excede la longitud permitida."
+//     // }
 
-});
+// });
+
+function validarDatos(){
+    var CURP = document.getElementById("curp").value;
+    var IDP = document.getElementById("idp").value;
+    if(CURP && IDP){
+        console.log(CURP,IDP);
+        alert("AHH PERRIN SI LE SABES ");
+        //window.open("URL");
+    }
+    else alert("nanai");
+    
+}
 
 let navbar = document.querySelector(".navbar");
 let navLinks = document.querySelector(".nav-links");
